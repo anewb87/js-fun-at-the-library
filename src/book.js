989e35b8dcb01caa4105d5825fa766246ed35035
Alexis Newby <alexis.newby@gmail.com>
2021-10-13 21:37:14 -0600

    Book- saveReview calculatePageCount writeBook

--- src/book.js
+++ src/book.js
@@ -2,19 +2,40 @@ function createTitle(title) {
   return `The ${title}`
 };
 
-class buildMainCharacter {
-  constructor (name, age, pronouns) {
-    this.name = name;
-    this.age = age;
-    this.pronouns = pronouns;
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
   }
+  return mainCharacter
 };
+
+function saveReview(criticReview) {
+  var reviews = [];
+  reviews.push(criticReview)
+};
+
+function calculatePageCount(title) {
+  return title.length * 20
+};
+
+function writeBook(createTitle, buildMainCharacter, calculatePageCount, genre) {
+  var book = {
+    title: createTitle,
+    mainCharacter: buildMainCharacter,
+    pageCount: calculatePageCount,
+    genre: genre,
+  }
+  return book
+}
+
 
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
+  saveReview,
+  calculatePageCount,
+  writeBook,
   // editBook
 }
