8fb11c8208d2950e7c137213257f0f24a4db83c6
Alexis Newby <alexis.newby@gmail.com>
2021-10-15 16:48:15 -0600

    Shelf: shelfBook, unshelfBook, listTitles, failing searchShelf

--- src/shelf.js
+++ src/shelf.js
@@ -7,16 +7,36 @@ function shelfBook(book, shelfArray) {
 function unshelfBook(title, shelfArray) {
   for (var i = 0; i < shelfArray.length; i++ ) {
     if (shelfArray[i].title === title) {
-      shelfArray.splice(i,1)
+      shelfArray.splice(i, 1)
     }
   }
 }
 
+function listTitles(shelfArray) {
+  var titleList = ''
+  for (var i = 0; i < shelfArray.length; i++ ) {
+    if (i < shelfArray.length - 1){
+      titleList += shelfArray[i].title + ", "
+    } else {
+    titleList += shelfArray[i].title
+    }
+  }
+  return titleList
+}
 
+function searchShelf(shelfArray, title) {
+  for (var i = 0; i < shelfArray.length; i++ ) {
+    if (shelfArray[i].title === title) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
 
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf
 };
