3ba8bccab2b3cb854b3677f1efa0bc98c32c5298
Alexis Newby <alexis.newby@gmail.com>
2021-10-15 08:26:00 -0600

    Shelf: shelfBook & unshelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,22 @@
+function shelfBook(book, shelfArray) {
+  if (shelfArray.length < 3) {
+      shelfArray.unshift(book);
+  }
+};
+
+function unshelfBook(title, shelfArray) {
+  for (var i = 0; i < shelfArray.length; i++ ) {
+    if (shelfArray[i].title === title) {
+      shelfArray.splice(i,1)
+    }
+  }
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
