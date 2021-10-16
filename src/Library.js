f4ad1accb341a2fc1ec78209700d0745ecd5c48d
Alexis Newby <alexis.newby@gmail.com>
2021-10-15 19:45:26 -0600

    Library: createLibrary, addBook

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,26 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library
+}
+
+function addBook(library, book) {
+  library.shelves[book.genre].push(book)
+}
+
+function checkoutBook (library, book) {
+  library.shelves[book.genre].pop(book)
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
