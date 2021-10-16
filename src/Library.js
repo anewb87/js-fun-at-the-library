c3dd80bbdd8e54456c685dad01f038043dc918e0
Alexis Newby <alexis.newby@gmail.com>
2021-10-16 09:12:20 -0600

    Library complete: revisit if possible

--- src/Library.js
+++ src/Library.js
@@ -14,19 +14,33 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book)
 }
 
-function checkoutBook (library, book, shelf) {
-  var checkOut = `You have now checked out ${book.title} from the ${library}`
-  var cannotCheckOut = `Sorry, there are currently no copies of ${book.title} available at the ${library}`
-  for (var i = 0; i < library.shelves[shelf].length; i ++) {
-    if (book === library.shelves[shelf][i].title) {
-      library.shelves[shelf].splice(i, 1);
-      return checkOut
-  } else {
-    return cannotCheckOut
-  }
-}
-}
+// function checkoutBook (library, bookTitle, specificShelf) {
+//   var checkOut = `You have now checked out ${bookTitle} from the ${library.name}`
+//   var cannotCheckOut = `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+//   for (var i = 0; i < library.shelves[specificShelf].length; i ++) {
+//     if (bookTitle === library.shelves[specificShelf][i].title) {
+//       library.shelves[specificShelf].splice(i, 1);
+//       return checkOut;
+//     } else {
+//       return cannotCheckOut;
+//     }
+//   }
+// }
 
+
+function checkoutBook (library, bookTitle, specificShelf) {
+  var checkOut = `You have now checked out ${bookTitle} from the ${library.name}`
+//  var cannotCheckOut = `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+  for (var i = 0; i < library.shelves[specificShelf].length; i ++) {
+    if (bookTitle === library.shelves[specificShelf][i].title) {
+      library.shelves[specificShelf].splice(i, 1);
+      return checkOut
+    }
+  }
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+}
+//this bottom one works, but I can't understand why the top one doesn't work.
+//I don't particuarly understand this one- throwing code at the wall- where I do understand the attempt above.
 
 
 
