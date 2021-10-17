e8664d55b2af4861f963d0797e2f217c6076076a
Alexis Newby <alexis.newby@gmail.com>
2021-10-17 14:54:31 -0600

    Work Librarian: findBook skeleton

--- src/librarian.js
+++ src/librarian.js
@@ -1,13 +1,23 @@
+
 class Librarian {
   constructor(name, library) {
     this.name = name;
     this.library = library;
-    //this.greetPatron = "Hello"
   }
-   greetPatron(patron) {
-     return "Hello, " + patron + "!";
+  greetPatron(patron, isMorning) {
+     if (isMorning === true) {
+       return "Good morning, " + patron + "!";
+     } else {
+       return "Hello, " + patron + "!";
+     }
+   }
+   findBook(bookTitle) {
+     if ( === bookTitle) {
+       //having trouble following the test: var book = ... DOES exist. But where is it? Need to 'find' it so I can access and see if it's the same as bookTitle
+     } else {
+       return "Sorry, we do not have " + bookTitle;
+     }
    }
 }
-
 
 module.exports = Librarian;
